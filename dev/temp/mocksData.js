fdf745dfcadcaade3fe78b72bd11f2ec68596250
Filipp Gaponenko <psr1919plus21@gmail.com>
2016-11-10 01:38:53 +0300

    :wrench: Remove dev from gitignore.

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,11 @@
+'head': {
+    defaults: {
+        title: 'default title',
+        useSocialMetaTags: true
+    }
+},
+
+__pages: [{
+                name: 'index',
+                href: 'index.html'
+             }]
