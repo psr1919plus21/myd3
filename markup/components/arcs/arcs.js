3dbdb0d1f2f2f5acfb71c96c65cccfa6964e941b
Filipp Gaponenko <psr1919plus21@gmail.com>
2017-01-29 00:48:14 +0300

    :rocket: Add piechart.

--- markup/components/arcs/arcs.js
+++ markup/components/arcs/arcs.js
@@ -1,8 +1,11 @@
 
 import * as d3 from 'd3';
 
-const height = 500;
-const width = 500;
+const data = [20, 40, 60];
+const height = 1500;
+const width = 1500;
+const r = 300;
+
 
 buildCanvas();
 
@@ -10,24 +13,50 @@ buildCanvas();
 function buildCanvas() {
 
     const canvas = d3.select('.arcs .l-restrictor')
-    .append('svg')
-    .attr('height', height)
-    .attr('width', width)
-    .append('g')
-    .attr('transform', 'translate(100, 100)');
+        .append('svg')
+        .attr('height', height)
+        .attr('width', width);
 
-    let r = 100;
-    let p = Math.PI * 2;
+    let color = d3.scaleOrdinal()
+        .range(['red', 'blue', 'yellow']);
+
+    let group = canvas.append('g')
+        .attr('transform', 'translate(500, 500)');
 
     let arc = d3.arc()
-        .innerRadius(r - 20)
-        .outerRadius(r)
-        .startAngle(0)
-        .endAngle(p - 1);
+        .innerRadius(150)
+        .outerRadius(r);
+
+    let pie = d3.pie()
+        .value( (d) => {
+            return d;
+        });
+
+    let arcs = group.selectAll('.arc')
+        .data(pie(data))
+        .enter()
+        .append('g')
+        .attr('class', 'arc');
+
+    arcs.append('path')
+        .attr('d', arc)
+        .attr('fill', (d) => {
+            return color(d.data);
+        });
+
+    arcs.append('text')
+        .attr('transform', (d) => {
+            return `translate( ${arc.centroid(d)} )`;
+        })
+        .attr('font-size', 22)
+        .attr('fill', '#333')
+        .attr('text-anchor', 'middle')
+        .text( (d) => {
+            return d.value;
+        });
 
 
-    canvas.append('path')
-        .attr('d', arc);
+
 
 
 }
